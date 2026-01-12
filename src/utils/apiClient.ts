c20a4b6c9584f0656e3675c72fb307d122922b3b
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-01-12 15:26:42 +0500

    fix: localStorage error in apiClient

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -12,22 +12,35 @@ export const apiClient = axios.create({
 });
 apiClient.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+    if (
+      typeof window !== "undefined" &&
+      window.localStorage
+      // typeof window.localStorage.getItem === "function"
+    ) {
+      const token = localStorage.getItem("token");
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
     }
     return config;
   },
-  (error) => Promise.reject(error),
+  (error) => Promise.reject(error)
 );
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      document.location.href = "./login";
-      localStorage.removeItem("token");
-      console.warn("Unauthorized, redirecting to login...");
+      if (
+        typeof window !== "undefined" &&
+        window.location &&
+        window.localStorage &&
+        typeof window.localStorage.removeItem === "function"
+      ) {
+        document.location.href = "./login";
+        localStorage.removeItem("token");
+        console.warn("Unauthorized, redirecting to login...");
+      }
     }
     return Promise.reject(error);
-  },
+  }
 );
