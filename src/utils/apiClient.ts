09143b36475fdd697208d9384aa2ec17c5d02d7f
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-04-21 20:01:50 +0500

    chore: update API base URLs and enable JSON content-type header in apiClient

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -6,9 +6,9 @@ const BUSE_URL =
 
 export const apiClient = axios.create({
   baseURL: BUSE_URL,
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 apiClient.interceptors.request.use(
   (config) => {
